46d0236df607825c65ae7106136b7e6e7d9e9220
Kooyeroon <kooyeroon2013@gmail.com>
2023-08-07 03:32:16 -0700

    modification

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,20 +6,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_class(unsigned char *e_ident);
-void check_elf(unsigned char *e_ident);
 void print_data(unsigned char *e_ident);
+void check_elf(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void close_elf(int elf);
 void print_version(unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
  * check_elf - Checks if a file to be ELF file.
- * @e_ident: A pointer to an array contains magic numbers.
+ * @e_ident: A pointer to an array contains magic nums.
  *
  * Description: If the file is not an ELF file - exit code is 98.
  */
