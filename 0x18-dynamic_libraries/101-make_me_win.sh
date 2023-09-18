59a9cc611ad02a8c1b5eefd01c00be8b90021a62
Kooyeroon <kooyeroon2013@gmail.com>
2023-09-18 02:17:26 -0700

    modified

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/Kooyeroon/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
-export LD_PRELOAD=../jackpot.so
+wget -P /tmp https://github.com/Kooyeroon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD = /tmp/libgiga.so
