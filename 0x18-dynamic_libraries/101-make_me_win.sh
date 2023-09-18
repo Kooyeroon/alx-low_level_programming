7217cdcaf3cb80189f43bd9f95da74f9c7505bb1
Kooyeroon <kooyeroon2013@gmail.com>
2023-09-18 05:38:46 -0700

    Fixing problem

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kooyeroon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD = /tmp/libgiga.so
+wget -P /tmp -q "https://github.com/Kooyeroon/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
+export LD_PRELOAD="/tmp/libmmw.so"
